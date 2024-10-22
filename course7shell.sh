59e770e8788d9bc0d403e2a97ac79c73bb6134a3
Akbar_Group 3 <exeaccrabka@gmail.com>
2024-10-22 20:31:27 +0800

    update code

--- course7shell.sh
+++ course7shell.sh
@@ -24,41 +24,30 @@ echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
 export REGION_1=${ZONE::-2}
 
-gcloud config set compute/region $REGION
-gcloud config set compute/zone $ZONE
-
-# Create VPC networks and subnets
 gcloud compute networks create managementnet --subnet-mode=custom
-gcloud compute networks subnets create managementsubnet-1-$REGION --network=managementnet --region=$REGION --range=10.130.0.0/20
+
+gcloud compute networks subnets create managementsubnet-1-$REGION_1 --network=managementnet --region=$REGION_1 --range=10.130.0.0/20
 
 gcloud compute networks create privatenet --subnet-mode=custom
-gcloud compute networks subnets create privatesubnet-1-$REGION --network=privatenet --region=$REGION --range=172.16.0.0/24
-gcloud compute networks subnets create privatesubnet-2-$REGION --network=privatenet --region=$REGION --range=172.20.0.0/20
 
-# Create firewall rules
+gcloud compute networks subnets create privatesubnet-1-$REGION_1 --network=privatenet --region=$REGION_1 --range=172.16.0.0/24
+
+gcloud compute networks subnets create privatesubnet-2-$REGION_2 --network=privatenet --region=$REGION_2 --range=172.20.0.0/20
+
 gcloud compute firewall-rules create managementnet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=managementnet --action=ALLOW --rules=icmp,tcp:22,tcp:3389 --source-ranges=0.0.0.0/0
 
 gcloud compute firewall-rules create privatenet-allow-icmp-ssh-rdp --direction=INGRESS --priority=1000 --network=privatenet --action=ALLOW --rules=icmp,tcp:22,tcp:3389 --source-ranges=0.0.0.0/0
 
-# Create instances
-gcloud compute instances create managementnet-vm-1 --zone=$ZONE --machine-type=e2-micro --subnet=managementsubnet-1-$REGION
+gcloud compute instances create	managementnet-vm-1 --zone=$ZONE --machine-type=e2-micro --subnet=managementsubnet-1-$REGION_1
 
-gcloud compute instances create privatenet-vm-1 --zone=$ZONE --machine-type=e2-micro --subnet=privatesubnet-1-$REGION
+gcloud compute instances create privatenet-1-vm --zone=$ZONE --machine-type=e2-micro --subnet=privatesubnet-1-$REGION_1
 
-# Create VPC peering
-gcloud compute network-peerings create peering-management-private \
-  --network=managementnet \
-  --peer-network=privatenet \
-  --export-routes-to-peer=ENABLE \
-  --import-routes-from-peer=ENABLE
-
-# Create an instance with multiple network interfaces
 gcloud compute instances create vm-appliance \
-  --zone=$ZONE \
-  --machine-type=e2-standard-4 \
-  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=managementsubnet-1-$REGION \
-  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=privatesubnet-1-$REGION \
-  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=mynetwork
+--zone=$ZONE \
+--machine-type=e2-standard-4 \
+--network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=privatesubnet-1-$REGION_1 \
+--network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=managementsubnet-1-$REGION_1 \
+--network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=mynetwork
 
 echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
 
